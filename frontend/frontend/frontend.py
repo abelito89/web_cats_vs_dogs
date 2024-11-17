303422f1cf14ccd627a8ed85103d60d35e1aec6a
Abel Gómez Méndez <abelmetaltele@gmail.com>
2024-11-17 17:12:08 -0500

    No se reproduce audio

--- frontend/frontend/frontend.py
+++ frontend/frontend/frontend.py
@@ -2,16 +2,27 @@ import reflex as rx
 from frontend.components.navbar import navbar
 from frontend.components.image_upload_button import upload_button
 from frontend.state.state import AppState
+import logging
 
 from rxconfig import config
 
 
+logging.basicConfig(level=logging.DEBUG)
+_logger = logging.getLogger(__name__)
+
 api_url = "http://127.0.0.1:8000/predict/"
 
-
+def audio_reproductor(audio):
+    _logger.debug(f"Reproduciendo audio: {audio}")  # Registro de prueba
+    print(f"Reproduciendo audio: {audio}")  # Agrega esta línea para ver si se llama en el navegador
+    return rx.audio(
+        src=f"/assets/audio/{audio}",
+        autoplay=True
+    )
 
 def index():
     """Vista principal de la aplicación."""
+    _logger.debug(f"Valor de AppState.prediction: {AppState.prediction}")
     return rx.vstack(
         upload_button(),  # Botón para subir imágenes
         rx.cond(
@@ -19,13 +30,30 @@ def index():
             rx.image(src=AppState.img),
         ),
         rx.cond(
-            (AppState.prediction != "") & (AppState.prediction == "cat"),
-            rx.text("Esto es un gato"),
-            rx.text("Esto es un perro"),
+            AppState.prediction != "",
+            rx.cond(
+                AppState.prediction == "cat",
+                rx.fragment(
+                    rx.text("Esto es un gato"),
+                    rx.audio(
+                        src="/assets/audio/maullido.mp3",
+                        autoplay=True
+                    )
+                ),
+                rx.fragment(
+                    rx.text("Esto es un perro"),
+                    rx.audio(
+                        src="/assets/audio/ladrido.mp3",
+                        autoplay=True
+                    )
+                )
+            ),
+            rx.text("Por favor, sube una imagen para obtener un resultado.")
         ),
         padding="5em",
     )
 
 
-app = rx.App(State=AppState)
+
+app = rx.App(State=AppState, config=config)
 app.add_page(index)
--- frontend/rxconfig.py
+++ frontend/rxconfig.py
@@ -2,4 +2,8 @@ import reflex as rx
 
 config = rx.Config(
     app_name="frontend",
+    frontend_port=3000,
+    backend_port=8000,
+    asset_dir="frontend/assets",  # Cambia según tu estructura
 )
+print(f"Serving static files from: {config.asset_dir}")
